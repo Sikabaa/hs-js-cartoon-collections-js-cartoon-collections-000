e4f1ff63344d1d1c209749e1e8f64579aadc5e1a
Mercy Atiemo <mercyatiemo@gmail.com>
2020-03-30 01:05:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function dwarfRollCall(dwarves) {for (let i=0; i<dwarves.length; i++) return i.length dwarves.shift
+function dwarfRollCall(i,dwarves) {for (let i=0; i<dwarves.length; i++) return i.length dwarves.shift
 }
 
 function summonCaptainPlanet(planeteerCalls){
