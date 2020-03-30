58a1e887d8f520b7bb2c21a6589c8531149790ab
Mercy Atiemo <mercyatiemo@gmail.com>
2020-03-30 03:08:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var dwarves = ['Dopey', 'Bashful', 'Grumpy',]
+var dwarves = ['Dopey', 'Bashful', 'Grumpy',];
+
 function dwarfRollCall(dwarves) {let listDwarves = [];
   for(let i = 0; i < dwarves.length; i++);{
     
